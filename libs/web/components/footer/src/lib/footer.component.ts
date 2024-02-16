e84995bcff26ca83597c4e9192d88556d7a55907
Owen Kelvin <okotieno@yahoo.com>
2024-02-16 10:07:23 +0300

    Profile

--- libs/web/components/footer/src/lib/footer.component.ts
+++ libs/web/components/footer/src/lib/footer.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JoinNewsletterComponent } from '@maisha/web/components/join-newsletter';
+import { IsActiveMatchOptions, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'furaha-footer',
   standalone: true,
-  imports: [CommonModule, JoinNewsletterComponent],
+  imports: [
+    CommonModule, JoinNewsletterComponent,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  routerLinkActiveOptions: IsActiveMatchOptions = {
+    fragment: 'exact',
+    paths: 'exact',
+    queryParams: 'exact',
+    matrixParams: 'exact'
+  }
+}
--- libs/web/components/team/src/lib/team/team.component.ts
+++ libs/web/components/team/src/lib/team/team.component.ts
@@ -18,32 +18,18 @@ export interface TeamMember {
 export class TeamComponent {
   teamMembers: TeamMember[] = [
     {
-      name: 'Jane Doe',
+      name: 'Abhishek Kansara',
       title: 'CEO & Founder',
-      description: 'Lorem ipsum dolor sit amet.',
-      email: 'example@example.com',
-      imageUrl: "assets/img/user.svg"
+      description: 'BDS (Hons) MFDS RCS (Eng) | DCT2 Restorative ',
+      email: 'abhishek@maishahealthkenya.co.ke',
+      imageUrl: "https://media.licdn.com/dms/image/C5603AQFY0yBu3sP9tg/profile-displayphoto-shrink_800_800/0/1544964726504?e=1713398400&v=beta&t=tVhW9Fs69ci0HtPga8pdwW38Yw7RrHp5z8vpcTZ2fI0"
     },
     {
-      name: 'John Doe',
-      title: 'Surgeon',
-      description: 'Lorem ipsum dolor sit amet.',
-      email: 'example@example.com',
-      imageUrl: "assets/img/user.svg"
+      name: 'Owen Kelvin',
+      title: 'CTO',
+      description: 'BBS Actuarial',
+      email: 'owen@maishahealthkenya.co.ke',
+      imageUrl: "https://media.licdn.com/dms/image/C5103AQE_PgQB_w77gQ/profile-displayphoto-shrink_800_800/0/1517427013563?e=1713398400&v=beta&t=d1FBFJ_UsVCC1_ybcOrlwLukim2siIP2SMdrigq33GQ"
     },
-    {
-      name: 'Mike Ross',
-      title: 'Doctor',
-      description: 'Lorem ipsum dolor sit amet.',
-      email: 'example@example.com',
-      imageUrl:"assets/img/user.svg"
-    },
-    {
-      name: 'John Doe',
-      title: 'Dentist',
-      description: 'Lorem ipsum dolor sit amet.',
-      email: 'example@example.com',
-      imageUrl: "assets/img/user.svg"
-    }
   ];
 }
