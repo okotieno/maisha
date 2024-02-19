7a670491330f62f9a4598c7d6f49e18a99da6ebf
Owen Kelvin <okotieno@yahoo.com>
2024-02-19 19:10:34 +0300

    Adds text

--- libs/web/pages/home/src/lib/home.component.ts
+++ libs/web/pages/home/src/lib/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { WebComponentsHeroComponent } from '@furaha/web/components/hero';
 import { WebComponentsAboutComponent } from '@furaha/web/components/about-us';
 import { WebComponentsClientsComponent } from '@furaha/web/components/clients';
@@ -7,6 +7,7 @@ import { ContactComponent } from '@furaha/contact';
 import { TeamComponent } from '@furaha/team';
 import { HealthLibraryComponent } from '@maisha/web/components/health-library';
 import { FooterComponent } from '@maisha/web/components/footer';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -23,4 +24,31 @@ import { FooterComponent } from '@maisha/web/components/footer';
   ],
   styleUrls: ['home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router, private elementRef: ElementRef) { }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    console.log({ event })
+
+    // Get all section elements
+    const sections = this.elementRef.nativeElement.querySelectorAll('section');
+
+    // Find the section in view
+    let activeSection = '';
+    sections.forEach((section: any) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+        activeSection = section.id;
+      }
+    });
+
+    // Change fragment based on active section
+    this.changeFragment(activeSection);
+  }
+
+  changeFragment(fragment: string) {
+    // Navigate to the given fragment
+    this.router.navigate([], { fragment: fragment });
+  }
+}
